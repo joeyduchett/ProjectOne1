c22f3c6f3f248f51c026ab7ef93aff8380b56ceb
michael mariea <mariea.michael@gmail.com>
2019-06-29 14:11:14 -0700

    files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-const firebaseConfig = {
-    apiKey: "AIzaSyB7ySPEV0gp6qFYWi0v_daSWIaAQbpOuh8",
-    authDomain: "newproject-be903.firebaseapp.com",
-    databaseURL: "https://newproject-be903.firebaseio.com",
-    projectId: "newproject-be903",
-    storageBucket: "newproject-be903.appspot.com",
-    messagingSenderId: "213280593884",
-    appId: "1:213280593884:web:36cc06648062fc8a"
-};
-firebase.initializeApp(firebaseConfig);
-
-
-const database = firebase.database();
-let clickCounter = 0;
-
-database.ref().set({
-  clickCount: clickCounter
-});
-database.ref().on("value", function(snapshot) {
-  console.log(snapshot.val());
-
-
-  clickCounter = snapshot.val().clickCount;
-
-  console.log(clickCounter);
-
-
-  $("#clicks").text(clickCounter);
-}, function(errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
-
-
-
-$("#submit").on("click", function() {
-  clickCounter++;
-  console.log(clickCounter);
-  database.ref().set({
-    clickCount: clickCounter
-  });
-
-});
-=======
 $(document).ready(function(){
 const config = {
     apiKey: "AIzaSyCM4JX2RT_DHRCdXGPZAK_AdbvR8AFnt7I",
@@ -106,4 +62,3 @@ const config = {
     console.log("The read failed: " + errorObject.code);
   });
 })
->>>>>>> c4d2e1a3579e2a6dcd70235a9f00f89911a27654
