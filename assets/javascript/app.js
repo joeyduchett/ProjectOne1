c8614c28d464594612a81727053c309172820f25
michael mariea <mariea.michael@gmail.com>
2019-07-02 18:57:41 -0700

    merged both click and counter functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  $("#click-button").on("click", function() {
+    console.log("working");
+    const track = $("#input-value").val().trim();
+    let queryURL = "http://ws.audioscrobbler.com/2.0/?method=track.search&api_key=5fb156e4bba3aaa310f7cfbd87f6cfdf&track=" + track + "&format=json";
+    console.log("Track:", track);
+    console.log("URL:", queryURL);
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response, "response");
+  });
+});
 const config = {
     apiKey: "AIzaSyCM4JX2RT_DHRCdXGPZAK_AdbvR8AFnt7I",
     authDomain: "the-one-d42f5.firebaseapp.com",
