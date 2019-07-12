58d76c3e88d1cc5f31b6a1f44f72bf154d2eced2
DylanF <dyl6999@hotmail.com>
2019-07-11 19:35:52 -0700

    added s

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
     $('table').empty();
     // console.log("working");
     const track = $("#input-value").val().trim();
-    let queryURL = "http://ws.audioscrobbler.com/2.0/?method=track.search&api_key=5fb156e4bba3aaa310f7cfbd87f6cfdf&track=" + track + "&limit=10&format=json";
+    let queryURL = "https://ws.audioscrobbler.com/2.0/?method=track.search&api_key=5fb156e4bba3aaa310f7cfbd87f6cfdf&track=" + track + "&limit=10&format=json";
     console.log("Track:", track);
     console.log("URL:", queryURL);
     $.ajax({
